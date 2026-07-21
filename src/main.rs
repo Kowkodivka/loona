375096470bb21a6357975832c87cd474ace68d90
Kowkodivka <86189625+Kowkodivka@users.noreply.github.com>
2026-07-21 14:21:12 +0700

    chore: remove todo

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,6 @@ static_loader! {
     };
 }
 
-// TODO: добавить настройки языка для отдельного пользователя
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
